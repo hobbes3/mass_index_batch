53643afd078837a11d453893ab2e781f7114477a
Satoshi Kawasaki <hobbes3@gmail.com>
2019-01-11 14:10:23 +0000

    First working mass index script with the new streaming/copying strategy. Also has auto-load and -save features.

--- mass_index.py
+++ mass_index.py
@@ -5,8 +5,7 @@ import os
 import time
 import sys
 import glob
-import subprocess
-import pexpect
+import csv
 import logging
 import logging.handlers
 from tqdm import tqdm
@@ -26,51 +25,61 @@ if __name__ == "__main__":
     logger = logging.getLogger("logger")
     logger.setLevel(logging.DEBUG)
     handler = logging.handlers.RotatingFileHandler(LOG_PATH, maxBytes=LOG_ROTATION_BYTES, backupCount=LOG_ROTATION_LIMIT)
-    handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)-7s] (%(threadName)-10s) %(message)s", datefmt="%Y-%m-%d %H:%M:%S"))
+    handler.setFormatter(logging.Formatter("%(asctime)s [%(levelname)-7s] %(message)s", datefmt="%Y-%m-%d %H:%M:%S"))
     logger.addHandler(handler)
 
     print("Log file at {}".format(LOG_PATH))
 
-    logger.info("START OF SCRIPT.")
-    logger.info("SLEEP={}, LIMIT={}, len(DATA)={}".format(SLEEP, LIMIT, len(DATA)))
+    logger.info("===START OF SCRIPT===")
+    logger.info("From settings.py: SLEEP={}, LIMIT={}, len(DATA)={}.".format(SLEEP, LIMIT, len(DATA)))
 
     data = []
 
-    for i, d in enumerate(DATA):
-        src = d["src"]
-        dst = d["dst"]
+    if os.path.exists(SAVED_FILE_LIST_PATH):
+        logger.info("Saved file list found! Reading {}.".format(SAVED_FILE_LIST_PATH))
+	
+        with open(SAVED_FILE_LIST_PATH) as csv_file:
+            reader = csv.DictReader(csv_file)
+            for r in reader:
+                data.append(r)
 
-        logger.debug("DATA #{}: src={} dst={}".format(i, src, dst))
+    else:
+        logger.info("Saved file list not found. Reading settings.py.")
+        for i, d in enumerate(DATA):
+            src = d["src"]
+            dst = d["dst"]
 
-        files = glob.glob(src)
+            files = glob.glob(src)
+            logger.debug("DATA #{}: src={}, dst={}, len(files)={}.".format(i, src, dst, len(files)))
 
-        data.extend([
-            {
-                "file": f,
-                "dst": dst,
-            }
-            for f in files
-        ])
+            data.extend([
+                {
+                    "file": f,
+                    "dst": dst,
+                }
+                for f in files
+            ])
 
     total = len(data)
-    logger.debug("{} files to index.".format(total))
+    logger.info("Total {} file(s).".format(total))
     
     pbar = tqdm(total=total)
 
     count = 0
+    count_retries = 0
 
     while len(data) > 0:
         total_files = 0
 
-	for d in DATA:
+        for d in DATA:
             dst = d["dst"]
-            count_files += len(os.walk(dst).next()[2])
+            count_files = len(glob.glob(dst + "*"))
             total_files += count_files
-            logger.debug("{}: {} has {} file(s)".format(count, dst, count_files))
+            logger.debug("{}: {} currently has {} file(s).".format(count, dst, count_files))
 
         diff = LIMIT - total_files
         
-        logger.debug("{}: total_files={}".format(count, total_files))
+        logger.info("{}: Total number of files found: {}.".format(count, total_files))
 
         if diff > 0:
             for i in range(diff):
@@ -80,9 +89,32 @@ if __name__ == "__main__":
 
                 copy2(f, dst)
                 logger.info("{}: Copied {} to {}.".format(count, f, dst))
+                tries = 0
                 pbar.update(1)
+        else:
+            count_retries += 1
+            logger.debug("{}: LIMIT={} reached. Retry attempt #{}.".format(count, LIMIT, count_retries))
+    
+        if count_retries > len(SLEEP):
+            logger.error("{}: No more retry attempts left. Saving remaining file list (length={}) to {}. ".format(count, len(data), SAVED_FILE_LIST_PATH))
             
-        count += 1
-        time.sleep(SLEEP)
+            with open(SAVED_FILE_LIST_PATH, "w") as csv_file:
+                fields = ["file", "dst"]
+                writer = csv.DictWriter(csv_file, fieldnames=fields)
+                writer.writeheader()
+                writer.writerows(data)
+            csv_file.close()
 
-    logger.info("DONE. Total elapsed seconds: {}".format(time.time() - start_time))
+            logger.info("INCOMPLETE. Total elapsed seconds: {}.".format(time.time() - start_time))
+            sys.exit("No more retry attempts left. See logs at {}.".format(LOG_PATH))
+
+        count += 1
+        sleep = SLEEP[count_retries-1]
+        logger.debug("{}: Sleeping for {} second(s).".format(count, sleep))
+        time.sleep(sleep)
+
+    if os.path.exists(SAVED_FILE_LIST_PATH):
+        logger.info("DONE. Deleting {}.".format(SAVED_FILE_LIST_PATH))
+        os.remove(SAVED_FILE_LIST_PATH)
+
+    logger.info("DONE. Total elapsed seconds: {}.".format(time.time() - start_time))
